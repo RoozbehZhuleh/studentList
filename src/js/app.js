b320a28f127ad6583069ba239f7d0f2e218281bb
dan salahi <dan.salahii@gmail.com>
2022-08-23 23:24:44 +0300

    added the base javaascript

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+const fullNameInput = document.querySelector("#fullName")
+const mobileNumberInput = document.querySelector("#mobileNumber")
+const emailInput = document.querySelector("#email")
+const searchPhraseInput = document.querySelector("#searchPhrase")
+
+
+
+
+class Student {
+    constructor() {
+
+    }
+}
+
+
+class RenderUI {
+    constructor() {
+
+    }
+}
+
+
+class InitApp {
+    constructor() {
+
+    }
+
+    init() {
+
+    }
+}
+
+
+const app = new InitApp()
+app.init()
